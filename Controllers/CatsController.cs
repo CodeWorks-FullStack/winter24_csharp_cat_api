236b1bb900e9fb87e8fdd87573ee4549b10cfefc
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-25 14:05:45 -0600

    test method worked!

---
+++ Controllers/CatsController.cs
@@ -0,0 +1,21 @@
+namespace csharp_cat_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+// [Route("api/cats")]
+public class CatsController : ControllerBase
+{
+
+  [HttpGet]
+  public ActionResult<string> TestApi()
+  {
+    try
+    {
+      return Ok("API IS UP, BABY");
+    }
+    catch (Exception error)
+    {
+      return BadRequest(error.Message);
+    }
+  }
+}
